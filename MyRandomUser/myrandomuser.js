48cac19ccc0d1c1fdd5014014f5981235a2a3631
Quoc Sy Luong <quocsyluong@MacBook-Pro-van-Quoc.local>
2017-05-18 13:16:09 +0200

    Commit it

--- MyRandomUser/myrandomuser.js
+++ MyRandomUser/myrandomuser.js
@@ -4,6 +4,8 @@
 
 var express = require('express');
 var app = express();
+// Opdracht 1.6
+var port = process.env.PORT || 3000;
 
 app.get('/', function(request, response) {
     response.send('Hello Avans!');
@@ -11,6 +13,21 @@ app.get('/', function(request, response) {
 
 app.get('/about', function(request, response) {
     response.send('Geschreven door Quocsy');
+})
+
+// JSON formaat ophalen met request
+app.get('/json', function(request, response) { response.json({
+    'some_name': 'Value', 'an_array_of_objects': [
+        {
+            'another_name': 'Another value', 'a_further_name': "A further value"
+        },
+        {'yet_another_name': 'Yet another value'} ],
+    'some_boolean': true, 'some_integer': 42, 'array_of_ints': [
+        2, 3, 5, 7, 11, 13
+    ],
+    'array_of_strings': [
+        "twee", "drie", "vijf", "zeven"
+    ] })
 })
 
 // Opdracht 1.2e
@@ -34,6 +51,6 @@ app.all('*', function(request, response) {
     response.send('404 - Not found');
 })
 
-app.listen(3000, function() {
-    console.log('Server app is listening on post 3000');
+app.listen(port, function() {
+    console.log('Server app is listening on port ' + port);
 })
